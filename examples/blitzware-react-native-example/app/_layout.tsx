96071bcdee21cd9a92c80409f498bc4fca0926e2
LanderDK <landerdekesel@gmail.com>
2025-09-23 21:29:52 +0200

    refactor: update BlitzWareAuthClient configuration and dependencies for improved cookie handling and session support

--- examples/blitzware-react-native-example/app/_layout.tsx
+++ examples/blitzware-react-native-example/app/_layout.tsx
@@ -8,7 +8,10 @@ import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
-import { BlitzWareAuthProvider, BlitzWareConfig } from "blitzware-react-native-sdk";
+import {
+  BlitzWareAuthProvider,
+  BlitzWareConfig,
+} from "blitzware-react-native-sdk";
 
 export const unstable_settings = {
   anchor: "(tabs)",
@@ -18,9 +21,9 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   const blitzWareConfig: BlitzWareConfig = {
-    clientId: "2f465572-9a90-4bd1-b4ec-3b03b33fbb66", // Test client ID
-    redirectUri: "blitzwarereactnativeexample://oauth", // Using the app scheme from app.json
-    responseType: "code", // or "token" for implicit flow
+    clientId: "your-client-id",
+    redirectUri: "yourapp://oauth", // Must match your app scheme
+    responseType: "code", // OAuth 2.0 authorization code flow
   };
 
   return (
